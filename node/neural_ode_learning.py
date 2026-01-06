b157584a905184fde90c283d28e03ba4a0fb56bb
bl4drnnr <mikhail.bahdashych@protonmail.com>
2026-01-06 14:59:35 +0100

    node final report

--- node/neural_ode_learning.py
+++ node/neural_ode_learning.py
@@ -1425,7 +1425,7 @@ def run_node(mode=None, model='small_mlp', epochs=500, batch_size=8, lr=1e-3, ts
 
 # %%
 # run_node(mode='generate_data', suffix='small_mlp_run')
-# run_node(mode='generate_data', suffix='large_mlp_run')
+run_node(mode='generate_data', suffix='large_mlp_run')
 
 # %% [markdown]
 # ### Example 2: Train Both Models (with suffix)
@@ -1440,7 +1440,7 @@ def run_node(mode=None, model='small_mlp', epochs=500, batch_size=8, lr=1e-3, ts
 
 # %%
 # run_node(mode='train', model='small_mlp', epochs=500, batch_size=256, lr=1e-3, suffix='small_mlp_run')
-# run_node(mode='train', model='large_mlp', epochs=1500, batch_size=256, lr=1e-3, suffix='large_mlp_run')
+run_node(mode='train', model='large_mlp', epochs=1500, batch_size=256, lr=1e-3, suffix='large_mlp_run')
 
 # %% [markdown]
 # ### Example 3: Evaluate Both Models
@@ -1451,8 +1451,8 @@ def run_node(mode=None, model='small_mlp', epochs=500, batch_size=8, lr=1e-3, ts
 # ```
 
 # %%
-run_node(mode='evaluate', model='small_mlp', suffix='small_mlp_run')
-# run_node(mode='evaluate', model='large_mlp', suffix='large_mlp_run')
+# run_node(mode='evaluate', model='small_mlp', suffix='small_mlp_run')
+run_node(mode='evaluate', model='large_mlp', suffix='large_mlp_run')
 
 # %% [markdown]
 # ### Example 4: Generate Visualizations
@@ -1462,8 +1462,8 @@ run_node(mode='evaluate', model='small_mlp', suffix='small_mlp_run')
 # ```
 
 # %%
-run_node(mode='visualize', suffix='small_mlp_run')
-# run_node(mode='visualize', suffix='large_mlp_run')
+# run_node(mode='visualize', suffix='small_mlp_run')
+run_node(mode='visualize', suffix='large_mlp_run')
 
 # %% [markdown]
 # ### Example 5: Complete Pipeline with Timestamp
